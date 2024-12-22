4a4a503fdbed44e0614a2bcf57067483ece6b8ab
TA2k <tombox2020@gmail.com>
2024-12-22 22:32:39 +0100

    fix for dots in array elements

--- src/index.ts
+++ src/index.ts
@@ -382,7 +382,7 @@ class Json2iob {
         }
         let arrayPath = key + index;
         if (typeof arrayElement === "string" && key !== "") {
-          await this.parse(path + "." + key + "." + arrayElement, arrayElement, options);
+          await this.parse(path + "." + key + "." + arrayElement.replace(/\./g, ""), arrayElement, options);
           continue;
         }
         if (typeof arrayElement[Object.keys(arrayElement)[0]] === "string") {
